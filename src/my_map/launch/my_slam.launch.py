7e3cd369da83a4d55aa5db301b2c1692fd293ecb
nttkor <nttkor@naver.com>
2026-01-20 20:50:14 +0900

    my_slam.launch.py

--- src/my_map/launch/my_slam.launch.py
+++ src/my_map/launch/my_slam.launch.py
@@ -26,7 +26,15 @@ def generate_launch_description():
             'base_frame': 'base_link',
             'odom_frame': 'odom',
             'scan_topic': '/scan',
-            'mode': 'mapping'
+            'mode': 'mapping',
+
+            # --- 해상도 및 맵 크기 관련 핵심 파라미터 ---
+            'resolution': 0.01,          # 0.05에서 0.01로 변경 (더 정밀하고 큰 지도)
+            'max_laser_range': 10.0,     # 레이저가 인식할 최대 거리
+            'minimum_time_interval': 0.5,
+            'transform_timeout': 0.2,
+            'tf_buffer_duration': 30.0,
+            'stack_size_to_use': 40000000 # 메모리 에러 방지용
         }]
     )
 
